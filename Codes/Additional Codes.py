72e09cd19b570e4b3b1a6eb06d2ea50714815f5b
daryltay336 <84765119+daryltay336@users.noreply.github.com>
2021-06-01 14:51:48 +0800

    Added square function

---
+++ Codes/Additional Codes.py
@@ -0,0 +1,14 @@
+import datetime
+import math
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+def square(x):
+    result = x**2
+    return result
+
+square(20)
+square(11)
+square(15)
